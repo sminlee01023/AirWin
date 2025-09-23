6e85ef575cb9dbf122daaeb8938b0abe9b4ec979
sminlee0123 <81750370+sminlee01023@users.noreply.github.com>
2025-09-23 17:27:27 +0900

    Add files via upload

---
+++ main/templates/flash_video.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meat charset='utf-8'>
+    <title>공기공기_플래시</title>
+    <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
+    <script defer src="https://pyscript.net/latest/pyscript.js"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap');
+        @import url(//fonts.googleapis.com/earlyaccess/nanumpenscript.css);
+        body{
+            background-color: rgb(221, 208, 245);
+        }
+    </style>
+
+    
+</head>
+</html>
